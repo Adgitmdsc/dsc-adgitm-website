4b9a7cd5d75b3aace1a24534177ae1a62eda8e12
Atharv jairath <54663702+atharvjairath@users.noreply.github.com>
2020-11-27 22:04:26 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import Blogimage from '../assets/blog.jpg';
+import Blogimage1 from '../assets/blog_1.jpg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -52,11 +53,10 @@ export default function Blog() {
             variant="contained"
             color="primary"
             className={classes.button}
-            link="http://medium.com/"
-            disabled
+            link="https://medium.com/dsc-adgitm"
+            target="_blank"
           >
-            {' '}
-            View on medium{' '}
+            View on medium
           </Button>
         </div>
       </div>
@@ -69,6 +69,42 @@ export default function Blog() {
           className={classes.gridContainer}
           justify="center"
         >
+          <Grid item xs={12} sm={6}>
+            <Card className={classes.root} variant="outlined">
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="300"
+                  image={Blogimage1}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                  Automate Your Instagram(Selenium) Part-1
+
+                  </Typography>
+                  <Typography variant="h5" color="textSecondary" component="p">
+                    Atharv Jairath
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+
+                <div align="center">
+                  <Button
+                    size="large"
+                    variant="contained"
+                    color="secondary"
+                    href="https://medium.com/@atharv.jairath/automate-your-instagram-selenium-361c1eaf17fe"
+                    target="_blank"
+                  >
+                    Read More
+                  </Button>
+                </div>
+              </CardActions>
+            </Card>
+          </Grid>
           <Grid item xs={12} sm={6}>
             <Card className={classes.root} variant="outlined">
               <CardActionArea>
