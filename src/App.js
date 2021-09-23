823ec2e7953ba6107ff0b4cd933232d1b2ac3ad2
Uday yadav <dev117uday@gmail.com>
2021-09-23 19:40:55 +0530

    Project Init with Bootstrap
    - added bootstrap 5 lib ( CDN )

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="container mt-3">
+      <h2 className="text-center">
+        Hello World
+      </h2>
+    </div>
+  );
+}
+
+export default App;
