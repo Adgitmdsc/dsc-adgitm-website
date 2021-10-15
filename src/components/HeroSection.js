2fc5a14a5a1f19058709148bbe91212438832858
Rajib Mondal <mondal021002rajib@gmail.com>
2021-10-15 14:07:23 +0530

    arrow animation and functionality added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -25,7 +25,7 @@ function HeroSection() {
                     />
                 </div>
             </Container>
-            <a style={{ cursor: 'pointer' }} href="#aimSection" id="aimSection"><img className="pb-4 pt-1 mb-4" src="https://img.icons8.com/external-those-icons-fill-those-icons/24/ffffff/external-down-arrows-those-icons-fill-those-icons-1.png" /></a>
+            <a style={{ cursor: 'pointer' }} href="#aimSection" id="aimSection"><img className="pb-4 pt-1 mb-4 arrow" src="https://img.icons8.com/external-those-icons-fill-those-icons/24/ffffff/external-down-arrows-those-icons-fill-those-icons-1.png" /></a>
 
         </>
     )
